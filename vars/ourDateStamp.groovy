e99d306c50b406ed6ca7abb630311ec84fb4cb10
Ihar <avattargrey@gmail.com>
2021-10-15 14:55:57 +0300

    Update ourDateStamp.groovy

--- vars/ourDateStamp.groovy
+++ vars/ourDateStamp.groovy
@@ -3,10 +3,10 @@ def call(format)
 def today = new Date()
    if (format == "ddmmyy" )
    { 
-     return today.format("dd/MM/yy")
+     return today.format("dd.MM.yy")
    }
    if (format == "mmddyy" )
    { 
-     return today.format("MM/dd/yy")
+     return today.format("MM.dd.yy")
    }
 }
