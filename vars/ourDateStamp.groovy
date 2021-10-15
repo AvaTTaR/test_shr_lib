a558fa80398222a349e88eb079aa77d95a434d35
Ihar <avattargrey@gmail.com>
2021-10-15 14:33:53 +0300

    Create ourDateStamp.groovy

---
+++ vars/ourDateStamp.groovy
@@ -0,0 +1,12 @@
+def call(format)
+{
+def today = new Date()
+   if (format = "ddmmyy" )
+   { 
+     println today.format("dd/MM/yy")
+   }
+   if (format = "mmddyy" )
+   { 
+     println today.format("MM/dd/yy")
+   }
+}
